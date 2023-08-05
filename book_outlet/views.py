2f1d9f861d0bb5e6f934915065e09ffa7fe09107
daniel <danielnobre09@gmail.com>
2023-08-05 13:50:20 -0300

    books links to detail page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Book
 
 # Create your views here.
@@ -8,4 +9,14 @@ def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling,
     })
